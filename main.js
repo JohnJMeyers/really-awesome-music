fb2f70bf1054311635acc28622a462c8900466b0
John Meyers <johnjmeyers1@gmail.com>
2017-06-18 17:26:18 -0500

    returns artists, add query selector on users and return tracks

--- main.js
+++ main.js
@@ -1,20 +1,72 @@
+let searchResults = document.querySelector('.search-results')
+
+
+
 function searchFor (formInput) {
+  let users = []
+
+
   fetch(`http://api.soundcloud.com/users/?client_id=8538a1744a7fdaa59981232897501e04&q=${formInput}`, {
     })
     .then( function(r) {
       return r.json()
     })
     .then( function(json) {
-      console.log(json)
-})
+      for (let i = 0; i < json.length; i++) {
+        let userInfo = {}
+        userInfo.id = json[i].id
+        userInfo.avatar = json[i].avatar_url
+        userInfo.userName = json[i].username
+
+        users.push(userInfo)
+
+    }
+      for (let i = 0; i < users.length; i++) {
+        
+        let userHTML = `
+        <section id='${users[i].id}' class='user-blocks'>
+          <div class='userImageDiv'>
+            <img class='userImage' src='${users[i].avatar}'>
+          </div>
+          <div class='userName'
+            <p>${users[i].userName}</p>
+          </div>
+        </section>`
+
+        searchResults.insertAdjacentHTML('beforeend', userHTML)
+
+        document.getElementById(`${users[i].id}`).addEventListener('click', function() {
+
+          getTracks(users[i].id, users[i].userName)
+        })
+
+      }
+
+    })
 
 }
 
+
+function getTracks(id, userName) {
+  // searchResults.textContent='';
+  fetch(`https://api.soundcloud.com/users/${id}/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f`, {
+  })
+  .then( function(p) {
+      return p.json()
+    })
+    .then( function(json) {
+      console.log(json);
+    })
+
+}
+
+
+
+
 document.querySelector('#search-button').addEventListener('click', function(event){
-
-
-  console.log("clicked");
+  event.preventDefault();
+  searchResults.textContent='';
   let formInput = document.querySelector('.formInput').value;
-  searchFor(formInput)
-  console.log(formInput)
+  searchFor (formInput)
+
 })
