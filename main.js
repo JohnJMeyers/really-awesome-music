6232b9efe10158fa0a83c5967213d1c0c36761fe
John Meyers <johnjmeyers1@gmail.com>
2017-06-20 13:44:24 -0500

    project complete

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-const token = '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f'
 let searchResults = document.querySelector('.search-results')
-let trackSong;
-
-
+let trackSong
 
 function searchFor (formInput) {
   let users = []
-
-
 
   fetch(`http://api.soundcloud.com/users/?client_id=8538a1744a7fdaa59981232897501e04&q=${formInput}`, {
     })
@@ -20,12 +15,9 @@ function searchFor (formInput) {
         userInfo.id = json[i].id
         userInfo.avatar = json[i].avatar_url
         userInfo.userName = json[i].username
-
         users.push(userInfo)
-
     }
       for (let i = 0; i < users.length; i++) {
-
         let userHTML = `
         <section id='${users[i].id}' class='user-blocks'>
           <div class='userImageDiv'>
@@ -38,25 +30,18 @@ function searchFor (formInput) {
 
         searchResults.insertAdjacentHTML('beforeend', userHTML)
 
-        document.getElementById(`${users[i].id}`).addEventListener('click', function() {
-
+        document.getElementById(`${users[i].id}`).addEventListener('click', function(event) {
+          event.preventDefault();
           getTracks(users[i].id, users[i].userName, users[i].avatar)
         })
-
       }
-
     })
-
 }
-
-
 function getTracks(id, userName, avatar) {
   let tracks = []
   searchResults.textContent='';
   fetch(`https://api.soundcloud.com/users/${id}/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f`, {
   })
-  // fetch(`https://api.soundcloud.com/users/${id}/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=${userName}`, {
-  // })
   .then( function(p) {
       return p.json()
     })
@@ -69,12 +54,8 @@ function getTracks(id, userName, avatar) {
         trackInfo.url = json[i].stream_url
         trackInfo.id = json[i].id
         trackInfo.avatar = avatar
-
         tracks.push(trackInfo)
-
-
       }
-
       for (let i = 0; i < tracks.length; i++) {
         let audio
         if (`${tracks[i].artwork}` === 'null') {
@@ -87,7 +68,7 @@ function getTracks(id, userName, avatar) {
               <p>${tracks[i].title}</p>
             </div>
           </section>`
-          console.log(tracks)
+
         } else {
           audio = `
           <section id='${tracks[i].url}' class='user-blocks'>
@@ -103,29 +84,23 @@ function getTracks(id, userName, avatar) {
         searchResults.insertAdjacentHTML('afterbegin', audio)
         document.getElementById(`${tracks[i].url}`).addEventListener('click', function() {
           document.querySelector('.audio-player').textContent='';
-          playTrack(tracks[i].url)
-
+          playTrack(tracks[i].url, tracks[i].title)
         })
       }
     })
-
 }
-function playTrack(track) {
+function playTrack(track, title) {
   let playSong = `
-  <audio  class='player' controls="true" class="player" src='${track}${token}' autoplay="true">
+  <audio class='player' controls="true" class="player" src='${track}?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f' autoplay="true">
 </audio>
+<p class='now-playing-text'>Now Playing: ${title}</p>
   `
   document.querySelector('.audio-player').insertAdjacentHTML('afterbegin', playSong)
 }
-
-
-
-
 
 document.querySelector('#search-button').addEventListener('click', function(event){
   event.preventDefault();
   searchResults.textContent='';
   let formInput = document.querySelector('.formInput').value;
   searchFor (formInput)
-
 })
